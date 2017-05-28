98d4f0f6f21c439fc1216bf0c903a0b10e5847d8
Nicolas Ganz <nicolas@keltec.ch>
2017-05-28 16:41:00 +0200

    added rspec-wait

--- rubys_cube.gemspec
+++ rubys_cube.gemspec
@@ -24,5 +24,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.15'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'rspec-wait', '~> 0.0'
   spec.add_development_dependency 'pry', '~> 0.10'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 require 'bundler/setup'
 require 'rubys_cube'
 require 'pry'
+require 'rspec/wait'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
+
+  config.wait_timeout = 3
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
