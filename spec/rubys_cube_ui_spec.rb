fc6e2fccc269140f76d72dca778a499394f92f77
Nicolas Ganz <nicolas@keltec.ch>
2017-05-28 16:55:04 +0200

    let ui tests be ignored if the env variable is set

--- spec/rubys_cube_ui_spec.rb
+++ spec/rubys_cube_ui_spec.rb
@@ -1,25 +1,27 @@
 require "spec_helper"
 
 RSpec.describe RubysCube do
-  before do
-    @cube = described_class.new
-    @cube.start_ui
-    wait_for do
-      @renderer = @cube.instance_variable_get(:@ui).
-        instance_variable_get(:@renderer)
-    end.not_to eq nil
-  end
+  unless ENV['SKIP_UI_TESTS'] == 'true'
+    before do
+      @cube = described_class.new
+      @cube.start_ui
+      wait_for do
+        @renderer = @cube.instance_variable_get(:@ui).
+          instance_variable_get(:@renderer)
+      end.not_to eq nil
+    end
 
-  after do
-    glfwSetWindowShouldClose window_handle, 1
-    @cube.wait_for_ui
-  end
+    after do
+      glfwSetWindowShouldClose window_handle, 1
+      @cube.wait_for_ui
+    end
 
-  it 'has the correct amount of boxes' do
-    expect(@renderer.instance_variable_get(:@_opengl_objects).size).to eq 6
-  end
+    it 'has the correct amount of boxes' do
+      expect(@renderer.instance_variable_get(:@_opengl_objects).size).to eq 6
+    end
 
-  def window_handle
-    @renderer.window.instance_variable_get(:@handle)
+    def window_handle
+      @renderer.window.instance_variable_get(:@handle)
+    end
   end
 end
